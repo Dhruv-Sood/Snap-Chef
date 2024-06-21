03c40d0b29f0e3ab6944eb301063ae0e5c27e728
Dhruv Sood <dhruvsoodboss@gmail.com>
2024-06-22 03:00:00 +0530

    Card update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({btn,desc,title}) => {
           <figure className=" overflow-hidden"><img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2hkcXY1cnMxZW1jcjFuZmYzejhwMWNqZGF3Mmdhdnd2emJlOTJjMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jKaFXbKyZFja0/giphy.gif" className="w-[800px]"/></figure>
           <div className="card-body">
               <h2 className="card-title">{title}</h2>
-              <p className="h-[120px]">{desc}nkjdbkdbcsbc</p>
+              <p className="h-[120px]">{desc}</p>
               <div className="card-actions justify-end">
                   <button className="btn btn-primary">{btn}</button>
               </div>
